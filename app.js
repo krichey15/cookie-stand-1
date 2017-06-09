a59d49b82bf44dfa3b91c08e9117140c79626552
nosrac77 <carson.newton@outlook.com>
2017-06-08 21:24:54 -0700

    added method to seatac object, still working on a typerror bug that wont let listItemElement print to html page

--- app.js
+++ app.js
@@ -15,43 +15,80 @@ var firstAndPikeLocation = {
 
 firstAndPikeLocation.generateRandomCookiesPerHour = function(min, max) {
   var randomCustPerHour = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log(firstAndPikeLocation.hourlyCookieSales);
   firstAndPikeLocation.hourlyCookieSales[1] = randomCustPerHour * Math.floor(firstAndPikeLocation.averageCookiesSoldPerCust);
   firstAndPikeLocation.hourlyCookieSales[0] = firstAndPikeLocation.hourlyCookieSales[0] + firstAndPikeLocation.hourlyCookieSales[1];
   return firstAndPikeLocation.hourlyCookieSales;
 };
 
-//below is a for loop I've created that loops through the hours of operation starting at 6am and displays both how many total cookies the store has sold AND how many cookies were sold in the last hour.
+//below is a for loop I've created that loops through the hours of operation starting at 6am and displays both how many total cookies the store has sold AND how many cookies were sold in the last hour. It also now prints html content to my sales.html page. It prints the same statement that I've told the console.log to by adding an li to my ul for every loop through the code.
 
-for (i = 0; i < 15; i++) {
+for (var i = 0; i < 15; i++) {
   firstAndPikeLocation.generateRandomCookiesPerHour(firstAndPikeLocation.minCustomersPerHour, firstAndPikeLocation.maxCustomersPerHour);
-  console.log('At ' + firstAndPikeLocation.hoursOfOperation[i] + ' total amount of cookies sold so far is ' + firstAndPikeLocation.hourlyCookieSales[0] + '. Total cookies sold this hour were ' + firstAndPikeLocation.hourlyCookieSales[1] + '.');
+  console.log('In the ' + firstAndPikeLocation.location + ' location, at ' + firstAndPikeLocation.hoursOfOperation[i] + ', total amount of cookies sold is ' + firstAndPikeLocation.hourlyCookieSales[0] + '. Total cookies sold this hour were ' + firstAndPikeLocation.hourlyCookieSales[1] + '.');
+
+  var firstAndPikeUlElement = document.getElementById('first-and-pike');
+
+  var listItemElement = document.createElement('li');
+
+  listItemElement.textContent = 'In the ' + firstAndPikeLocation.location + ' location, at ' + firstAndPikeLocation.hoursOfOperation[i] + ', total amount of cookies sold is ' + firstAndPikeLocation.hourlyCookieSales[0] + '. Total cookies sold this hour were ' + firstAndPikeLocation.hourlyCookieSales[1] + '.';
+
+  firstAndPikeUlElement.appendChild(listItemElement);
+
 }
 
 var seatacLocation = {
   location : 'Seatac International Airport',
   minCustomersPerHour : 3,
   maxCustomersPerHour : 24,
-  averageCookiesSoldPerCust : 1.2
+  averageCookiesSoldPerCust : 1.2,
+  hoursOfOperation : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  hourlyCookieSales: [0]
 };
+
+seatacLocation.generateRandomCookiesPerHour = function(min, max) {
+  var randomCustPerHour = Math.floor(Math.random() * (max - min + 1)) + min;
+  seatacLocation.hourlyCookieSales[1] = randomCustPerHour * Math.floor(seatacLocation.averageCookiesSoldPerCust);
+  seatacLocation.hourlyCookieSales[0] = seatacLocation.hourlyCookieSales[0] + seatacLocation.hourlyCookieSales[1];
+  return seatacLocation.hourlyCookieSales;
+};
+
+for (var i = 0; i < 15; i++) {
+  seatacLocation.generateRandomCookiesPerHour(seatacLocation.minCustomersPerHour, seatacLocation.maxCustomersPerHour);
+  console.log('In the ' + seatacLocation.location + ' location, at ' + seatacLocation.hoursOfOperation[i] + ', total amount of cookies sold is ' + seatacLocation.hourlyCookieSales[0] + '. Total cookies sold this hour were ' + seatacLocation.hourlyCookieSales[1] + '.');
+
+  var seatacUlElement = document.getElementById('seatac');
+
+  var listItemElement = document.createElement('li');
+
+  listItemElement.textContent = 'In the ' + seatacLocation.location + ' location, at ' + seatacLocation.hoursOfOperation[i] + ', total amount of cookies sold is ' + seatacLocation.hourlyCookieSales[0] + '. Total cookies sold this hour were ' + seatacLocation.hourlyCookieSales[1] + '.';
+
+  seatacUlElement.appendChild(listItemElement);
+
+}
 
 var seattleCenterLocation = {
   location : 'Seattle Center',
   minCustomersPerHour : 11,
   maxCustomersPerHour : 38,
-  averageCookiesSoldPerCust : 3.7
+  averageCookiesSoldPerCust : 3.7,
+  hoursOfOperation : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  hourlyCookieSales: [0]
 };
 
 var capitolHillLocation = {
   location : 'Capitol Hill',
   minCustomersPerHour : 20,
   maxCustomersPerHour : 38,
-  averageCookiesSoldPerCust : 2.3
+  averageCookiesSoldPerCust : 2.3,
+  hoursOfOperation : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  hourlyCookieSales: [0]
 };
 
 var alkiLocation = {
   location : 'Alki Beach',
   minCustomersPerHour : 2,
   maxCustomersPerHour : 16,
-  averageCookiesSoldPerCust : 4.6
+  averageCookiesSoldPerCust : 4.6,
+  hoursOfOperation : ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  hourlyCookieSales: [0]
 };
