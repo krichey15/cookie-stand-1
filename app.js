f53b7aed58f0bdc4f58da371a800d9245c0e4f08
nosrac77 <carson.newton@outlook.com>
2017-06-10 18:17:32 -0700

    separated thead, tbody, and tfoot code, made functions more dry. tfoot function is the final piece.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//READ ME: consider changing the functionality of the generateRandomCookiesPerHour function to store the values in long arrays that will hold each store's hourlyCookieSales, not just a tiny array with 2 indeces. This will allow you to use your footer function to easily grab the hourlyCookieSales data for each store to calculate the hourly rate across all stores (which is what you need to complete your table).
+
 function Store(location, minCustomersPerHour, maxCustomersPerHour, averageCookiesSoldPerCust, hoursOfOperation, hourlyCookieSales) {
   this.location = location;
   this.minCustomersPerHour = minCustomersPerHour;
@@ -32,19 +34,28 @@ var generateRandomCookiesPerHour = function(store) {
   return store.hourlyCookieSales;
 };
 
+//below is a function that creates my table header information
+
 var theadEl = document.getElementById('hours-of-operation');
-var trTheadEl = document.createElement('tr');
-theadEl.appendChild(trTheadEl);
 
 var printTableHeadingHours = function(store) {
+
+  var hoursTd = document.createElement('td');
+  hoursTd.textContent = '';
+  theadEl.appendChild(hoursTd);
   for (var i = 0; i < store.hoursOfOperation.length; i++) {
     var tdHeadEl = document.createElement('td');
     tdHeadEl.textContent = store.hoursOfOperation[i];
-    trTheadEl.appendChild(tdHeadEl);
+    theadEl.appendChild(tdHeadEl);
   }
+  var totalPerLocation = document.createElement('td');
+  totalPerLocation.textContent = 'Total for Location';
+  theadEl.appendChild(totalPerLocation);
 };
 
 printTableHeadingHours(firstAndPikeLocation);
+
+//below is the code for getting the table body information rendered.
 
 var tbodyEl = document.getElementById('store-information-table');
 
@@ -57,21 +68,37 @@ var printCookieSales = function(store) {
     var storeInfoRow = document.createElement('td');
     storeInfoRow.textContent = store.hourlyCookieSales[1];
     trEl.appendChild(storeInfoRow);
+
   }
+
 };
 
 for (var i = 0; i < storesList.length; i++) {
   var trEl = document.createElement('tr');
+
+  var storeNameTable = document.createElement('td');
+  storeNameTable.textContent = storesList[i].location;
+  trEl.appendChild(storeNameTable);
+
   printCookieSales(storesList[i]);
+
+  var totalCookiesSoldSlot = document.createElement('td');
+  totalCookiesSoldSlot.textContent = storesList[i].hourlyCookieSales[0];
+  trEl.appendChild(totalCookiesSoldSlot);
   tbodyEl.appendChild(trEl);
 }
 
+//below is unfinished code for the footer function, which will render the footer information of the table.
+
 var tFootEl = document.getElementById('totals');
 var tFootTrEl = document.createElement('tr');
+tFootEl.appendChild(tFootTrEl);
 
-for (var i = 0; i < storesList.length; i++) {
-  var storeTotalsRow = document.createElement('td');
-  storeTotalsRow.textContent = 'Total Cookies Sold for ' + storesList[i].location + ' is ' + storesList[i].hourlyCookieSales[0];
-  tFootTrEl.appendChild(storeTotalsRow);
-  tFootEl.appendChild(tFootTrEl);
+var createFooter = function(store){
+  var hourlyTotalRow = document.createElement('td');
+  hourlyTotalRow.textContent = 'Totals Per Hour';
+  tFootTrEl.appendChild(hourlyTotalRow);
+
+  //somehow create a process in which the stored values of the hourlyCookieSales properties for each separate store are added together per hour.
+
 };
